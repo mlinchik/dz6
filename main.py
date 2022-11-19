9b9efa80a98c993395c5d9b1b94aa933e8edd0c7
sasha <sasha.govorova100@gmail.com>
2022-11-19 12:17:23 +0200

    dz66

---
+++ main.py
@@ -0,0 +1,24 @@
+result = print("mew")
+def divider(a, b):
+ while True:
+  try:
+   result = int(input(divider))
+  except ValueError:
+   print("mew")
+  except IndexError:
+   print("mew")
+  except list:
+   print("mew")
+
+  if a < b:
+   raise ValueError
+  if b > 100:
+   raise IndexError
+   return a / b
+ data = {10: 2, 2: 5, "123": 4, 18: 0, []: 15, 8: 4}
+ for key in data:
+  res = divider(key, data[key])
+  result.append(res)
+
+
+print(result)
